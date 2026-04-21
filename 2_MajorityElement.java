fc523fbae4497767a52965c24c497e5d00eef90d
Aditya <anandaditya2718@gmail.com>
2026-04-21 14:45:29 +0530

    Day1 Majority Element Solved

--- 2_MajorityElement.java
+++ 2_MajorityElement.java
@@ -1,10 +1,23 @@
-class Solution {
-    public int majorityElement(int[] nums) {
+import java.util.*;
+
+public class MajorityElement {
+
+    public static int majorityElement(int[] nums) {
         Arrays.sort(nums);
-        return nums[nums.length/2];
+        return nums[nums.length / 2];
+    }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+        int[] nums = new int[n];
 
-        
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int result = majorityElement(nums);
+        System.out.println(result);
     }
 }
