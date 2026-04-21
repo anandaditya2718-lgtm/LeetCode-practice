d6cd2bec93748d9a37c766128119e20fdbbe82c3
Aditya <anandaditya2718@gmail.com>
2026-04-21 14:46:02 +0530

    Day1 Remove Duplicate

--- 3_RemoveDuplicate.java
+++ 3_RemoveDuplicate.java
@@ -1,15 +1,35 @@
-class Solution {
-    public int removeDuplicates(int[] nums) {
-        int count =0;
-        for(int i=0; i<nums.length;i++){
-            if(i<nums.length-1 && nums[i] == nums[i+1]){
+import java.util.*;
+
+public class RemoveDuplicates {
+
+    public static int removeDuplicates(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i < nums.length - 1 && nums[i] == nums[i + 1]) {
                 continue;
+            } else {
+                nums[count] = nums[i];
+                count++;
             }
-            else{
-                 nums[count] = nums[i];
-            count ++;
-        }
         }
         return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int k = removeDuplicates(nums);
+
+        System.out.println(k);
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
     }
 }
