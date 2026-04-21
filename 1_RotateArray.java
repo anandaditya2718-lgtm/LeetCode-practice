8557d250259a2db5384df7a508878712b337bfbb
Aditya <anandaditya2718@gmail.com>
2026-04-21 14:28:18 +0530

    Day1 Rotate Array Solved

--- 1_RotateArray.java
+++ 1_RotateArray.java
@@ -3,7 +3,7 @@ class Solution {
         int n = nums.length;
         k = k%n;
         int[] temp = new int [n];
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){   
             temp[(i+k)%n] = nums[i];
         }
         for(int i=0;i<n;i++){
