20c0ea243558dbe6e61f76c3de168278162d602d
Aditya <anandaditya2718@gmail.com>
2026-04-21 14:44:41 +0530

    Day1 Rotate Array Solved

--- 1_RotateArray.java
+++ 1_RotateArray.java
@@ -1,15 +1,38 @@
-class Solution {
-    public void rotate(int[] nums, int k) {
+import java.util.*;
+
+public class RotateArray {
+
+    public static void rotate(int[] nums, int k) {
         int n = nums.length;
-        k = k%n;
-        int[] temp = new int [n];
-        for(int i=0;i<n;i++){   
-            temp[(i+k)%n] = nums[i];
+        k = k % n;
+
+        int[] temp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            temp[(i + k) % n] = nums[i];
         }
-        for(int i=0;i<n;i++){
+
+        for (int i = 0; i < n; i++) {
             nums[i] = temp[i];
         }
-        
-       
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int k = sc.nextInt();
+
+        rotate(nums, k);
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
     }
 }
