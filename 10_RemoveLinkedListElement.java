8c1c6eaf53e083e2eab144b98bd2aba0e50b53d1
Aditya <anandaditya2718@gmail.com>
2026-04-27 14:25:36 +0530

    Day3 LinkedList Element removed

---
+++ 10_RemoveLinkedListElement.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class Main {
+
+    public static ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode current = dummy;
+
+        while (current.next != null) {
+            if (current.next.val == val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return dummy.next;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of nodes: ");
+        int n = sc.nextInt();
+
+        System.out.println("Enter elements:");
+        ListNode head = null, tail = null;
+
+        for (int i = 0; i < n; i++) {
+            int val = sc.nextInt();
+            ListNode node = new ListNode(val);
+
+            if (head == null) {
+                head = tail = node;
+            } else {
+                tail.next = node;
+                tail = node;
+            }
+        }
+
+        System.out.print("Enter value to remove: ");
+        int val = sc.nextInt();
+
+        head = removeElements(head, val);
+
+        System.out.println("Updated List:");
+        printList(head);
+
+        sc.close();
+    }
+}
