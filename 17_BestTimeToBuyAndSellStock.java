b7661e063667c704c875a8888939a1667adc9433
Aditya <anandaditya2718@gmail.com>
2026-04-28 09:36:54 +0530

    day3 best time to buy and sell stock solved

---
+++ 17_BestTimeToBuyAndSellStock.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+
+    
+    public static int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else {
+                maxProfit = Math.max(maxProfit, price - minPrice);
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.print("Enter number of days: ");
+        int n = sc.nextInt();
+
+        
+        int[] prices = new int[n];
+        System.out.println("Enter stock prices:");
+        for (int i = 0; i < n; i++) {
+            prices[i] = sc.nextInt();
+        }
+
+        
+        int result = maxProfit(prices);
+
+        
+        System.out.println("Maximum Profit: " + result);
+
+        sc.close();
+    }
+}
